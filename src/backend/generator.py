2eaff4e2eff689a2a6334b7f8d55bb6c91070331
abgese <abraham.g.sebastian@gmail.com>
2015-11-13 11:12:55 +0530

    Remove relation attribute ddl

--- src/backend/generator.py
+++ src/backend/generator.py
@@ -43,12 +43,12 @@ for entity in entity_list:
 		if(attribute["isPK"] == "True"):
 			blocks.append("PRIMARY KEY")
 			flag = True
-		if(attribute["CHECK"] != ""):
-			blocks.append("CHECK(" + attribute["CHECK"] + ")")
-			flag = True
-		if(attribute["DEFAULT"] != ""):
-			blocks.append("DEFAULT " + attribute["DEFAULT"])
-			flag = True
+		# if(attribute["CHECK"] != ""):
+		# 	blocks.append("CHECK(" + attribute["CHECK"] + ")")
+		# 	flag = True
+		# if(attribute["DEFAULT"] != ""):
+		# 	blocks.append("DEFAULT " + attribute["DEFAULT"])
+		# 	flag = True
 		if(flag):
 			chunks.append(' '.join(blocks))
 			mflag = True
@@ -72,51 +72,51 @@ for relation in relation_list:
 	chunks.append("ADD FOREIGN KEY ("+FK+")\n")
 	chunks.append("REFERENCES "+table2+"("+PK+");")
 	ddl.append(''.join(chunks))
-	# create table for relation attributes
-	if len(relation["attributes"])>0:
-    		chunks = []
-    		chunks.append("CREATE TABLE " + relation["name"] + "\n(\n")
-    		for attribute in relation["attributes"]:
-        			chunks.append(attribute["name"] + " " + attribute["datatype"])
-        			chunks.append(",\n")
-    		# remove last comma
-    		chunks.pop()
-    		chunks.append("\n)")
-   		ddl.append(''.join(chunks))
-   		chunks = []
-		mflag = False
-		chunks.append("ALTER TABLE " + entity["name"] + "\n")
-		#add constraints to table
-		for attribute in relation["attributes"]:
-			flag = False
-			blocks = []
-			blocks.append("MODIFY " + attribute["name"] + " " + attribute["datatype"])
-			if(attribute["notNULL"] == "True"):
-				blocks.append("NOT NULL")
-				flag = True
-			if(attribute["isUNIQUE"] == "True"):
-				blocks.append("UNIQUE")
-				flag = True
-			if(attribute["isPK"] == "True"):
-				blocks.append("PRIMARY KEY")
-				flag = True
-			if(attribute["CHECK"] != ""):
-				blocks.append("CHECK(" + attribute["CHECK"] + ")");
-				flag = True
-			if(attribute["DEFAULT"] != ""):
-				blocks.append("DEFAULT " + attribute["DEFAULT"]);
-				flag = True
-			if(flag):
-				chunks.append(' '.join(blocks))
-				mflag = True
-			else:
-				chunks.pop()
-			chunks.append(",\n")
-		# remove last comma
-		chunks.pop()
-		chunks.append(";")
-		if(mflag):
-			ddl.append(' '.join(chunks))
+# create table for relation attributes
+	# if len(relation["attributes"])>0:
+    		# chunks = []
+     		# chunks.append("CREATE TABLE " + relation["name"] + "\n(\n")
+    		# for attribute in relation["attributes"]:
+        			# chunks.append(attribute["name"] + " " + attribute["datatype"])
+        			# chunks.append(",\n")
+ # remove last comma
+    		# chunks.pop()
+    		# chunks.append("\n)")
+   		# ddl.append(''.join(chunks))
+   		# chunks = []
+		# mflag = False
+		# chunks.append("ALTER TABLE " + entity["name"] + "\n")
+#add constraints to table
+		# for attribute in relation["attributes"]:
+		# 	flag = False
+		# 	blocks = []
+		# 	blocks.append("MODIFY " + attribute["name"] + " " + attribute["datatype"])
+		# 	if(attribute["notNULL"] == "True"):
+		# 		blocks.append("NOT NULL")
+		# 		flag = True
+		# 	if(attribute["isUNIQUE"] == "True"):
+		# 		blocks.append("UNIQUE")
+		# 		flag = True
+		# 	if(attribute["isPK"] == "True"):
+		# 		blocks.append("PRIMARY KEY")
+		# 		flag = True
+		# 	if(attribute["CHECK"] != ""):
+		# 		blocks.append("CHECK(" + attribute["CHECK"] + ")");
+		# 		flag = True
+		# 	if(attribute["DEFAULT"] != ""):
+		# 		blocks.append("DEFAULT " + attribute["DEFAULT"]);
+		# 		flag = True
+		# 	if(flag):
+		# 		chunks.append(' '.join(blocks))
+		# 		mflag = True
+		# 	else:
+		# 		chunks.pop()
+		# 	chunks.append(",\n")
+# remove last comma
+		# chunks.pop()
+		# chunks.append(";")
+		# if(mflag):
+		# 	ddl.append(' '.join(chunks))
 
 
 for d in ddl:
